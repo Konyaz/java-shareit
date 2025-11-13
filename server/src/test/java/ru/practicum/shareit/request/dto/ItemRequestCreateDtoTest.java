5d641fb21425c93c27f086e17128898dccd97150
Konyaz <fvp@pkzdrav.ru>
2025-11-13 21:00:28 +0300

    feat: отладка гитхаб экшен

--- server/src/test/java/ru/practicum/shareit/request/dto/ItemRequestCreateDtoTest.java
+++ server/src/test/java/ru/practicum/shareit/request/dto/ItemRequestCreateDtoTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.json.JsonContent;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
